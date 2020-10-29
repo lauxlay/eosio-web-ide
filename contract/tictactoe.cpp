4986e2f1317e36feb7a09b542b600d753809335b
lauxlay <lauxlayjr@gmail.com>
2020-10-29 18:20:14 +0000

    tictactoe

--- contract/tictactoe.cpp
+++ contract/tictactoe.cpp
@@ -4,10 +4,11 @@ using namespace eosio;
 
 class [[eosio::contract]] tictactoe : public contract {
   public:
-
     using contract::contract;
 
     [[eosio::action]] void welcome( name host, name challenger ) {
-        require_auth( _self() );
+        require_auth( get_self() );
+        
+        eosio::print("Welcome, challengers", host, "and ", challenger, " !")
     }
 };
